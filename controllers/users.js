d2ca2c0b5902b208abaa3e8fc4b0945c30a6b1c1
Matt Thompson <matt.thompson@stack.com>
2017-11-11 14:15:34 -0500

    added deactivate

--- controllers/users.js
+++ controllers/users.js
@@ -55,6 +55,22 @@ var users 	= {
 			}
 		});
 	},
+	deactivate: 	function(req,res){
+		User.find({email:req.body.email}).exec(function(err,obj){
+			if (err) {
+				res.status(500).send({error: "Could not retrieve object"});
+			} else {
+				User.findByIdAndUpdate(obj.id, { $set: {"active":0} }, function (err, obj) {
+					if (err) {
+						res.status(500).send({error: "Error updating obj"});
+					} else {
+						res.send(obj);
+					}
+				});
+			}
+		});
+	},
+	}
 	delete: 		function(req,res){
 		User.find({_id: req.params.id}).remove().exec(function(err,obj){
 			if (err) {
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/users', 			users.all);
 router.get('/user/:id', 		users.one);
 router.post('/user', 			users.create);
 router.post('/user/find', 		users.find);
+router.post('/user/deactivate', users.deactivate);
 router.put('/user/:id', 		users.update);
 router.delete('/user/:id', 		users.delete);
 
